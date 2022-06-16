00796c1bd0aceb65837db4552d2ae91043658664
Cera665 <deletedunreadxoxo@gmail.com>
2022-06-16 12:54:49 -0400

    Objects etc

--- array-filtering.js
+++ array-filtering.js
@@ -1,14 +1,6 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-var filtered = numbers.filter()
- function filter = numbers.filter(function (numbers) {  
-       return number % 2 === 0
-     })  
-
-
-console.log(filtered)
-   
-   
-   
-   
-   
-
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const filtered = numbers.filter(evenNumbers);
+  function evenNumbers (number) {
+    return number % 2 === 0
+  };
+console.log(numbers.filter(evenNumbers));
--- looping-through-arrays.js
+++ looping-through-arrays.js
@@ -1,3 +1,12 @@
-const pets = ['cat','dog', 'rat']
+var pets = ['cat','dog', 'rat']
+  for (i in pets) { pets[i] = pets[i] + 's'}
 
-for i in pets { pets[i] = pets[i] + 's'}
+console.log(pets);
+
+/*
+    const pets = ['cat', 'dog', 'rat']
+    
+    for (let i = 0; i < pets.length; i++) {
+      pets[i] = pets[i] + 's'
+    }
+*/
